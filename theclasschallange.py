832287b9a3bd4595f727d91866cf4dc1365cdb93
Robert <nebuchadenezzar@gmail.com>
2021-12-08 20:30:43 +0100

    Class():

--- theclasschallange.py
+++ theclasschallange.py
@@ -1,8 +1,10 @@
 #bank account challange
 
+
+
 class Account():
 
-    def __init__(self, owner, balance):
+    def __init__(self, owner="John Doe", balance=0):
         self.owner = owner
         self.balance = balance
         # print("Account owner: ", owner, "\nAccount balance: ", balance)
@@ -12,19 +14,22 @@ class Account():
 
     def deposit(self, dep_amount):
         self.balance += dep_amount
+        print("Added {} to the balance".format(dep_amount))
 
     def withdrawal(self, wit_amount):
-        if wit_amount < self.balance:
+        if wit_amount <= self.balance:
             self.balance -= wit_amount
             print("Withdrawal accepted!")
         else:
             print("Not enough on your balance, Dear Sir, to withdraw PLN {}.".format(wit_amount))
 
-
+print(Account)
+print(Account())
 acct1 = Account("Rob", 1990)
 print(acct1)
 print(acct1.owner)
 print(acct1.balance)
-acct1.deposit(69)
+acct1.deposit(2001)
 print(acct1)
 acct1.withdrawal(1000)
+print(acct1)
