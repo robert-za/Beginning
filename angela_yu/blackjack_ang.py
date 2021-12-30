af652aa646eb556550c14ef83456a83fd00a86f8
Robert <nebuchadenezzar@gmail.com>
2021-12-30 19:06:35 +0100

    some bugs included.

--- angela_yu/blackjack_ang.py
+++ angela_yu/blackjack_ang.py
@@ -19,22 +19,22 @@ def calculate_score(hand):
 def print_hand_score(hand):
     if hand == player_hand:
         print(f"    Your cards: {player_hand}, final score: {player_score}")
-    elif hand == card_dealer_hand:
+    if hand == card_dealer_hand:
         print(f"    Computer's hand: {card_dealer_hand}, final score: {card_dealer_score}")
 
 def print_dealer_card():
     print(f"    Computer's first card is: {card_dealer_hand[0]}")
 
-def check_score():
-    if player_score == 21 and card_dealer_score < 21:
+def check_score(score1, score2): #zmienić na return, zmienić na parametry
+    if score1 == 21 and score2 < 21:
         print("You win. :)")
-    elif player_score <= 21 and player_score > card_dealer_score:
+    elif score1 <= 21 and score1 > score2:
         print("You win. :)")
-    elif player_score <= 21 and (card_dealer_score > 21):
+    elif score1 <= 21 and (score2 > 21):
         print("You win. :)")
-    elif player_score == 21 and card_dealer_score == 21:
+    elif score1 == 21 and score2 == 21:
         print("Draw. :|")
-    elif player_score == card_dealer_score:
+    elif score1 == score2:
         print("Draw. :|")
     else:
         print("You lose. :(")
@@ -43,45 +43,42 @@ def check_score():
 ######################################GAME#################################################
 ###########################################################################################
 
-if input("Do you want to play a Black Jack game? y or n: ") == "y":
-    play_game = True
-    player_score = 0
-    card_dealer_score = 0
-    player_hand = [deal_card(), deal_card()]
-    card_dealer_hand = [deal_card(), deal_card()]
-
+play_game = True
+player_score = 0
+card_dealer_score = 0
+player_hand = [deal_card(), deal_card()]
+card_dealer_hand = [deal_card(), deal_card()]
+os.system("clear")
 print(logo)
 player_score = calculate_score(player_hand)
 card_dealer_score = calculate_score(card_dealer_hand)
-if player_score == 21:
-    while card_dealer_score <= 17:
-            card_dealer_hand.append(deal_card())
-            card_dealer_score = calculate_score(card_dealer_hand)
-    
 
 print_hand_score(player_hand)
 print_dealer_card()
 
 while play_game:
     if input("Type 'y' to get another card, type 'n' to pass: ") == "y":
+        os.system("clear")
+        print(logo)
         player_hand.append(deal_card())
         player_score = calculate_score(player_hand)
-        if player_score > 21:
+        if player_score >= 21:
             print_hand_score(player_hand)
             print_hand_score(card_dealer_hand)
-            check_score()
+            check_score(player_score, card_dealer_score)
             play_game = False
-            os.system("clear")
+            # os.system("clear")
         else:
             print_hand_score(player_hand)
             print_dealer_card()
     else:
         player_score = calculate_score(player_hand)
         print_hand_score(player_hand)
-        while card_dealer_score <= 17:
+        while card_dealer_score < 17:
             card_dealer_hand.append(deal_card())
             card_dealer_score = calculate_score(card_dealer_hand)
         print_hand_score(card_dealer_hand)
-        check_score()
+        check_score(player_score, card_dealer_score)
         play_game = False
-        os.system("clear")
+        # os.system("clear")
+
