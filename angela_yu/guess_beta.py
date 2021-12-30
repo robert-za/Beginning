7334082cc60a761070e8f4a49c09d364647d5bbc
Robert <nebuchadenezzar@gmail.com>
2021-12-30 20:41:27 +0100

    need debugging

---
+++ angela_yu/guess_beta.py
@@ -0,0 +1,46 @@
+import random, os
+
+os.system("clear")
+print("Welcome to the high/low guess game!\nI'm thinking about a number" 
+" between 1 and 100.")
+
+the_number = random.randint(1,10)
+difficulty_list = ["easy", "hard"]
+difficulty = ""
+
+while difficulty not in difficulty_list:
+    difficulty = input("Do you choose 'hard' or 'easy' mode?: ")
+if difficulty == "easy":
+    lifes = 10
+else:
+    lifes = 5
+
+player_guess = ""
+game_on = True
+
+def check_number(guess, number, life):
+    if guess < number:
+        print("secret number is higher")
+        life -= 1
+        if life == 0:
+            game_on = False
+        print(f"lifes remaining: {lifes}")
+    elif guess > number:
+        print("secret number is lower")
+        life -= 1
+        if life == 0:
+            game_on = False
+        print(f"lifes remaining: {lifes}")
+    else:
+        print(f"Congratulations. The number is: {the_number}. You win with {lifes}" 
+        " lifes remaining.")
+        game_on = False
+
+
+    return game_on, life
+
+
+
+while game_on:
+    player_guess = int(input("Guess a number: "))
+    check_number(player_guess, the_number, lifes)
