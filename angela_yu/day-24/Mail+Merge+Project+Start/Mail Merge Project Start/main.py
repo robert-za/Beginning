0eb364ce2aaa0250e65ad8c40999fafb69a1322e
Robert <nebuchadenezzar@gmail.com>
2022-01-19 18:55:30 +0100

    v.1.1.

--- angela_yu/day-24/Mail+Merge+Project+Start/Mail Merge Project Start/main.py
+++ angela_yu/day-24/Mail+Merge+Project+Start/Mail Merge Project Start/main.py
@@ -7,12 +7,12 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-names_file = open("./Input/Names/invited_names.txt", "r")
-names = (names_file.readlines())
+with open("./Input/Names/invited_names.txt", "r") as names_file:
+    names = (names_file.readlines())
 
+with open("./Input/Letters/starting_letter.txt", "r") as starting_letter:
+        contents = starting_letter.read()
 for name in names:
     clear_name = name.strip("\n")
-    with open("./Input/Letters/starting_letter.txt", "r") as starting_letter:
-        contents = starting_letter.read()
-        with open(f"./Output/ReadyToSend/letter_for_{clear_name}.txt", mode = "w") as file:
-            file.write(contents.replace("[name]", clear_name))
+    with open(f"./Output/ReadyToSend/letter_for_{clear_name}.txt", mode = "w") as file:
+        file.write(contents.replace("[name]", clear_name))
