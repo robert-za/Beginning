9403c3c75b53a706353b9bc6e645e7e060ab98b6
Robert <nebuchadenezzar@gmail.com>
2022-01-20 20:36:31 +0100

    us_states_guessing_game.v.1.0.

--- angela_yu/day-25/main/us-states-game-start/main.py
+++ angela_yu/day-25/main/us-states-game-start/main.py
@@ -1,17 +1,34 @@
 import turtle
+import pandas as pd
 
 screen = turtle.Screen()
 screen.title("US States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
-
 turtle.shape(image)
 
-answer_state = screen.textinput(title = "Guess the State", prompt = "What's another state's name?")
+data = pd.read_csv("50_states.csv")
+all_states = data.state.to_list()
 
+guessed_states = []
 
-
-
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title = f"{len(guessed_states)}/50", prompt = "What's another state's name?").title()
+    
+    if answer_state == "Exit":
+        df = pd.DataFrame(all_states)
+        df.to_csv("missing_states.csv")
+        break
+    
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        answer_state_data = data[data.state == answer_state]
+        t.goto(int(answer_state_data.x), int(answer_state_data.y))
+        t.write(answer_state)
+        all_states.remove(answer_state)
 
 
 # def get_mouse_click_coor(x, y):
@@ -19,4 +36,5 @@ answer_state = screen.textinput(title = "Guess the State", prompt = "What's anot
 # turtle.onscreenclick(get_mouse_click_coor)
 # turtle.mainloop()
 
-screen.exitonclick()
+# screen.exitonclick()
+
