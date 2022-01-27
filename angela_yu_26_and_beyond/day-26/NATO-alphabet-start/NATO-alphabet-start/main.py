3182d3d68c8c6c9379d6a706227d23816e7c7fd8
Robert <nebuchadenezzar@gmail.com>
2022-01-27 21:37:29 +0100

    try_except_update

--- angela_yu_26_and_beyond/day-26/NATO-alphabet-start/NATO-alphabet-start/main.py
+++ angela_yu_26_and_beyond/day-26/NATO-alphabet-start/NATO-alphabet-start/main.py
@@ -1,15 +1,15 @@
 import pandas
 abc = pandas.read_csv("nato_phonetic_alphabet.csv")
-# for (index, row) in abc.iterrows():
-    # print(row.letter, row.code)
+
 alphabet = {row.letter:row.code for (index, row) in abc.iterrows()}
 
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 user_word = input("What's the word to translate to NATO code?: ").upper()
-output_list = [alphabet[letter] for letter in user_word]
-print(output_list)
-
-
-# for (letter, code) in alphabet.items():
-#     print(letter, code)
+while True:
+    try:
+        output_list = [alphabet[letter] for letter in user_word]
+    except KeyError:
+        print("sorry, not a word")
+        user_word = input("What's the word to translate to NATO code?: ").upper()
+    else:
+        print(output_list)
+        break
